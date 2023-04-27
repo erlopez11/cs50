c8f1dc03958d8537beaeb11328533f6c1ed91699
Your Nameerlopez11 <lopezelainy@gmail.com>
2023-04-27 12:17:36 -0500

    add second attemt at rotate function. Condotionl still not working

--- problem_sets/caesar/script.js
+++ problem_sets/caesar/script.js
@@ -20,7 +20,6 @@ function rotate(message, key) {
         for (let j = 0; j < alphabet.length; j++) {
             if (message[i] === alphabet[j]) {
                 cypherLetter = alphabet[(j + key) % 26];
-                console.log(cypherLetter);
                 cypherText += cypherLetter;
             } 
         }
@@ -28,4 +27,38 @@ function rotate(message, key) {
     return cypherText;
 }
 
-console.log(rotate('hello.', 1));
+
+
+//Possible alternative function? //
+
+function rotate2(message, key) {
+    alphaLower = 'abcdefghijklmnopqrstuvwxyz';
+    alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    speacialChar = ' .,;:!&';
+    cypherText = '';
+
+    for (let i = 0; i < message.length; i++) {
+        if (message[i] === message[i].toUpperCase()) {
+            for (let j = 0; j < alphaUpper.length; j++) {
+                if(message[i] === alphaUpper[j]) {
+                    cypherText += alphaUpper[(j + key) % 26];
+                }
+            }
+        } else if (message[i] === message[i].toLowerCase()) {
+            for (let m = 0; m < alphaLower.length; m++) {
+                if (message[i] === alphaLower[m]) {
+                    cypherText += alphaLower[(m + key) % 26];
+                }
+            }
+        } else if (message[i] !== alphaUpper.toUpperCase() && message[i] !== alphaLower.toLowerCase()) {
+            for (let p = 0; p < speacialChar.length; p++) {
+                if (message[i] === speacialChar[p]) {
+                    cypherText += speacialChar[p];
+                }
+            }
+        }
+    }
+    return cypherText;
+}
+
+console.log(rotate2('hEllo WoRlD!', 1));
